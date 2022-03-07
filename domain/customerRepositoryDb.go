f3e9207751732603c923ff3a5c915e9213620be1
Victor Bassey <viktorbasi545@gmail.com>
2022-03-07 12:39:07 +0200

    Implement database adapter

---
+++ domain/customerRepositoryDb.go
@@ -0,0 +1,48 @@
+package domain
+
+import (
+	"database/sql"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type CustomerRepositoryDb struct {
+	db *sql.DB
+}
+
+func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
+
+	findAllSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers"
+
+	rows, err := d.db.Query(findAllSql)
+	if err != nil {
+		log.Println("Error while querying customer table " + err.Error())
+		return nil, err
+	}
+
+	customers := make([]Customer, 0)
+	for rows.Next() {
+		var c Customer
+		err := rows.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateofBirth, &c.Status)
+		if err != nil {
+			log.Println("Error while scanning customers " + err.Error())
+			return nil, err
+		}
+		customers = append(customers, c)
+	}
+	return customers, nil
+}
+
+func NewCustomerRepositoryDb() CustomerRepositoryDb {
+	db, err := sql.Open("mysql", "user:password@tcp(localhost:3306)/dbname")
+	if err != nil {
+		panic(err)
+	}
+	
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	return CustomerRepositoryDb{db}
+}
